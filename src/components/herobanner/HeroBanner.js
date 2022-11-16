8b0e27ef7388cd511dde0c32236c5a10c23cf1b4
theresa <l33theresa@gmail.com>
2022-11-15 21:41:10 -0500

    finished homepage

--- src/components/herobanner/HeroBanner.js
+++ src/components/herobanner/HeroBanner.js
@@ -8,8 +8,8 @@ class HeroBanner extends React.Component {
                 <section className="afg-hero-banner__wrapper position-relative">
                     <div className="afg-hero-banner">
                         <img src={StageImage} alt="Almost Famous GTA band logo" className="w-100" />
-                        <div className="container py-5">
-                            <h1 className="text-center">Almost Famous Upcoming Shows <span className="afg-hero-banner__seo">Almost Famous GTA</span></h1>
+                        <div className="container py-5 px-2">
+                            <h1 className="text-center position-relative">Upcoming Live Shows</h1>
                         </div>
                     </div>
                 </section>
--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -15,8 +15,15 @@ import CuchulainnImage from "../assets/Capture.jpg";
 import HonestLawyer2Image from "../assets/honest-lawyer.jpg";
 import EIFImage from "../assets/EFC-Win2.jpg";
 import DakotaImage from "../assets/52598845_304819366882662_412119888920838144_o-1.jpg";
+import RoyalLegionImage from "../assets/royal-101-990x556.png";
+import BlackWolfImage from "../assets/Black-Wolf-973x556.jpg";
+import DrumFlatImage from "../assets/Drums-N-Flats-1.jpg";
+import HorseShoeImage from "../assets/Web-banner_AF_Horseshoe-Tavern_May-30-18.jpg";
+import Spot1Image from "../assets/2017-12-28_15-37-06.jpg";
+import CuchulainnCharityImage from "../assets/22815548_1688411837845013_4358704672892183037_n.jpg";
+import HamiltonBullDogsImage from "../assets/hamilton-bulldogs-erie-otters.jpg";
 
-const imagePerRow = 4;
+const imagePerRow = 8;
 
 const Homepage = () => {
 	const upcomingGigs = [
@@ -141,12 +148,113 @@ const Homepage = () => {
 			date: "2019-12-14"
 		},
 		{
-			heading:"Nov 16th (2019) – Almost Famous at Dakota’s Sports Bar & Grill",
+			heading:"November 16th, 2019 – Almost Famous at Dakota’s Sports Bar & Grill",
 			subHeading: "Dakota's sports bar, $5 Cover",
 			address: "2814 Lake Shore Blvd, W Unit 2, Toronto, ON",
 			description: "Show starts at 9pm",
 			image: "/assets/b1d1a42e902763d6e7de.jpg",
 			date: "2019-11-16"
+		},
+		{
+			heading:"November 2nd, 2019 – Almost Famous at Cuchulainn’s Irish Pub",
+			subHeading: "Cuchulainn’s Irish Pub, No cover",
+			phone: "(905) 821-3790",
+			address: "158 Queen St S, Mississauga, Ontario L5M 1K8",
+			description: "Show starts at 9pm",
+			image: "/assets/84eb8df840a46d6450c0.jpg",
+			date: "2019-11-02"
+		},
+		{
+			heading:"October 5th, 2019 – Almost Famous at Cuchulainn’s Irish Pub",
+			subHeading: "Cuchulainn’s Irish Pub, No cover",
+			phone: "(905) 821-3790",
+			address: "158 Queen St S, Mississauga, Ontario L5M 1K8",
+			description: "Show starts at 9pm",
+			image: "/assets/84eb8df840a46d6450c0.jpg",
+			date: "2019-10-05"
+		},
+		{
+			heading:"September 7th, 2019 – Almost Famous at Cuchulainn’s Irish Pub",
+			subHeading: "Cuchulainn’s Irish Pub, No cover",
+			phone: "(905) 821-3790",
+			address: "158 Queen St S, Mississauga, Ontario L5M 1K8",
+			description: "Show starts at 9pm",
+			image: "/assets/84eb8df840a46d6450c0.jpg",
+			date: "2019-09-07"
+		},
+		{
+			heading:"August 17th, 2019 – Almost Famous at Cuchulainn’s Irish Pub",
+			subHeading: "Cuchulainn’s Irish Pub, No cover",
+			phone: "(905) 821-3790",
+			address: "158 Queen St S, Mississauga, Ontario L5M 1K8",
+			description: "Show starts at 9pm",
+			image: "/assets/84eb8df840a46d6450c0.jpg",
+			date: "2019-08-17"
+		},
+		{
+			heading:"August 10th, 2019 – Almost Famous Dinner/Dance with Rock Express",
+			subHeading: "Doors open 5:30pm – Show starts at 6:00pm – Dinner at 7:00pm",
+			address: "Royal Canadian Legion, Branch 101",
+			description: "$35 Advanced $40 at the door, Ticket information",
+			image: "/assets/4f18dc8758dfa5d6e376.png",
+			date: "2019-08-10"
+		},
+		{
+			heading:"July 6th, 2019 – Almost Famous at Dakota’s Sports Bar & Grill",
+			subHeading: "Dakota's sports bar, $5 Cover",
+			address: "2814 Lake Shore Blvd, W Unit 2, Toronto, ON",
+			description: "Show starts at 9pm",
+			image: "/assets/b1d1a42e902763d6e7de.jpg",
+			date: "2019-07-06"
+		},
+		{
+			heading:"June 22nd, 2019 – Almost Famous at The Black Wolf Smokehouse",
+			subHeading: "The Black Wolf Smokehouse, $5 Cover",
+			address: "139 Broadway Ave, Orangeville, ON",
+			phone: "519-941-1313",
+			description: "Show starts at 9pm",
+			image: "http://localhost:8080/assets/2f5243cf5c68402294a0.jpg",
+			date: "2019-06-22"
+		},
+		{
+			heading:"August 24th, 2018 – Almost Famous at Drums N Flats",
+			subHeading: "Drums N Flat, $5 Cover",
+			address: "60 Randall Drive, Ajax, ON",
+			description: "Show starts at 9pm",
+			image: "/assets/81b6490c8eddddee3a6d.jpg",
+			date: "2018-08-24"
+		},
+		{
+			heading:"May 30th, 2018 – Almost Famous at The Horseshoe Tavern",
+			subHeading: "Other acts: Hyness, Saint Yves, Six At Best, Infinite Repeat",
+			address: "370 Queen Street West, Toronto, ON",
+			description: "Show starts at 9pm. We go on at 11:30pm.",
+			image: "/assets/1a440dccc088c2db7119.jpg",
+			date: "2018-05-30"
+		},
+		{
+			heading:"January 27th, 2018 – Almost Famous at Spot 1 Grill & Music Hall",
+			subHeading: "Other acts: Torpedo Sunrise, Mid Life Crisis",
+			address: "289 Rutherford Rd South, Brampton, ON",
+			description: "Show starts at 9pm. We go on at 11pm.",
+			image: "/assets/5866f2657c2b61f18603.jpg",
+			date: "2018-01-27"
+		},
+		{
+			heading:"December 9th, 2017 – Almost Famous at Cuchulainn’s Irish Pub, Charity Event",
+			subHeading: "Other acts: The Groove Co, Ryan Dornan, Luke Reece, Poetry Slam",
+			address: "158 Queen St South, Mississauga, ON",
+			description: "Event starts at 8pm. We go on at 10:30pm.",
+			image: "/assets/fe4925b5849f3feab283.jpg",
+			date: "2017-10-09"
+		},
+		{
+			heading:"November 10th, 2017 – Almost Famous at FirstOntario Centre",
+			subHeading: "We will be playing at the FirstOntario Centre in the King Club (bar area) from 6:30 pm to 7:00 pm and during the 2 intermissions.",
+			address: "101 York Blvd, Hamilton, ON",
+			description: "6:00 PM",
+			image: "/assets/9b34a261377bc32994dd.jpg",
+			date: "2017-11-10"
 		},
 	];
 
